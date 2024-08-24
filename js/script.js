b7950580647df47f7ccbd650540785dcb3a00f09
gasukaku <94212316+gasukaku@users.noreply.github.com>
2024-08-25 00:23:22 +0900

    トップページ反映

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ function writeHeader(rootDir){
         }
     });
 }
+
+function writeFooter(rootDir){
+  $.ajax({
+       url : "../js/footer.html",
+        cache: false,
+        async: false,
+        success: function(html){
+
+            html = html.replace(/\{\$root\}/g, rootDir);
+            document.write(html);
+        }
+    });
+}
+
