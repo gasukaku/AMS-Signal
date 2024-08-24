993bcc94a66546ca290a3e4611789edc6c55e8ca
gasukaku <94212316+gasukaku@users.noreply.github.com>
2024-08-25 00:09:52 +0900

    一旦コミット

--- js/script.js
+++ js/script.js
@@ -10,15 +10,3 @@ function writeHeader(rootDir){
         }
     });
 }
-function writeFooter(rootDir){
-  $.ajax({
-        url: "/js/footer.html",
-        cache: false,
-        async: false,
-        success: function(html){
-
-            html = html.replace(/\{\$root\}/g, rootDir);
-            document.write(html);
-        }
-    });
-}
