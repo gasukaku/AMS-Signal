6de7501e824e9426a80f56ac52d8a0263aec9cf1
gasukaku <94212316+gasukaku@users.noreply.github.com>
2024-08-24 23:59:44 +0900

    js-test

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 fetch('../js/header.html')
   .then(response => response.text())
   .then(data => {
-    const headerElement = document.getElementById('header');
+    const headerElement = document.getElementById('commonheader');
     headerElement.innerHTML = data;
   });
 
